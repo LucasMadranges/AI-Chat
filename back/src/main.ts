0e57417ac0daf00f9543f3bac2cf8e61eada6310
Lucas Madranges <lucas.madranges.pro@icloud.com>
2025-04-09 16:48:35 +0200

    feat(database): integrate TypeORM and add database module
    Introduced TypeORM with a DatabaseModule and related providers for PostgreSQL integration. Also added Swagger support for API documentation and updated dependencies in package.json.

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,11 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
+import {SwaggerModule, DocumentBuilder} from "@nestjs/swagger";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.use(helmet());
     app.enableCors();
+
+    const config = new DocumentBuilder()
+        .setTitle('Chat AI')
+        .setDescription('API Docs pour Chat AI')
+        .setVersion('1.0')
+        .addTag('AI')
+        .build();
+    const documentFactory = () => SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('docs', app, documentFactory);
+    
     await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
