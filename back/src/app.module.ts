4f051545f8c049879ebe1e3694c3840bba6bb02f
Lucas Madranges <lucas.madranges.pro@icloud.com>
2025-04-09 16:49:07 +0200

    refactor(back): apply consistent formatting and cleanup
    Improved code readability by standardizing imports, spacing, and formatting across multiple files. Removed redundant code and adjusted indentation in `tsconfig.json`, module declarations, and entity setup for maintainability. These changes enhance consistency without affecting functionality.

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,12 +1,11 @@
-import {Module} from "@nestjs/common";
-import {AppController} from "./app.controller";
-import {AppService} from "./app.service";
-import {DatabaseModule} from "../libs/database/database.module";
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { DatabaseModule } from '../libs/database/database.module';
 
 @Module({
     imports: [DatabaseModule],
     controllers: [AppController],
     providers: [AppService],
 })
-export class AppModule {
-} 
+export class AppModule {}
--- back/src/main.ts
+++ back/src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
-import {SwaggerModule, DocumentBuilder} from "@nestjs/swagger";
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
@@ -16,7 +16,7 @@ async function bootstrap() {
         .build();
     const documentFactory = () => SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('docs', app, documentFactory);
-    
+
     await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
