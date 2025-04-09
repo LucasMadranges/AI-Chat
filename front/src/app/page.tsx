5f3c43a994ce230e7fb82b6a48d6843b96c6a580
Lucas Madranges <lucas.madranges.pro@icloud.com>
2025-04-09 14:43:24 +0200

    Test commit

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -5,6 +5,7 @@ export default function Home(){
   console.log("Ceci déclenche souvent une erreur de lint");
   
   const test = "test";
+  const test2 = "test";
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
