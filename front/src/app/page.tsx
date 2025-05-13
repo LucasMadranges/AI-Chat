9f70ce31514e3cd846c90a94792d0f19b65d6a10
Lucas Madranges <lucas.madranges.pro@icloud.com>
2025-05-13 09:44:47 +0200

    refactor(app, components, hooks): clean up and modularize message handling
    streamlined message handling by abstracting logic into a custom hook. replaced inline input
    and button components with reusable components for better maintainability.

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,18 +1,11 @@
 'use client';
 import ChatHistory from '@/components/ChatHistory';
-import { useState } from 'react';
-import api from '@/libs/api/axiosConfig';
+import Input from '@/components/Input';
+import Button from '@/components/Button';
+import { useSubmitMessage } from '@/libs/hooks/useSubmitMessage';
 
 export default function Home() {
-  const [message, setMessage] = useState('');
-
-  async function handleSubmitIA() {
-    const res = await api.post(`/message`, {
-      message,
-    });
-
-    setMessage(res.data);
-  }
+  const { message, setMessage, handleSubmitIA } = useSubmitMessage();
 
   return (
     <>
@@ -24,33 +17,13 @@ export default function Home() {
             De quoi voulez-vous discuter ?
           </h1>
           <div className={'relative flex items-center gap-4'}>
-            <input
-              type={'text'}
-              className={'border border-gray-200 rounded-lg p-2 shadow my-8 w-full'}
-              placeholder={'Rechercher...'}
+            <Input
               value={message}
-              onChange={e => setMessage(e.target.value)}
-              onSubmit={handleSubmitIA}
+              setValue={setMessage}
+              handleSubmit={e => handleSubmitIA(e)}
+              placeholder={'Rechercher...'}
             />
-            <button
-              className={
-                'bg-blue-500 rounded-full p-2 hover:bg-blue-600 cursor-pointer transition shadow active:scale-95 active:shadow-none'
-              }
-              onClick={handleSubmitIA}
-              onSubmit={handleSubmitIA}
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="1.5em"
-                height="1.5em"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  fill="#fff"
-                  d="m11 8.8l-2.9 2.9q-.275.275-.7.275t-.7-.275t-.275-.7t.275-.7l4.6-4.6q.3-.3.7-.3t.7.3l4.6 4.6q.275.275.275.7t-.275.7t-.7.275t-.7-.275L13 8.8V17q0 .425-.288.713T12 18t-.712-.288T11 17z"
-                />
-              </svg>
-            </button>
+            <Button handleSubmit={e => handleSubmitIA(e)} />
           </div>
         </div>
       </div>
--- front/src/libs/hooks/useSubmitMessage.ts
+++ front/src/libs/hooks/useSubmitMessage.ts
@@ -1,11 +1,13 @@
 'use client';
-import { useState } from 'react';
+import { FormEvent, useState } from 'react';
 import api from '@/libs/api/axiosConfig';
 
 export function useSubmitMessage() {
   const [message, setMessage] = useState('');
 
-  async function handleSubmitIA() {
+  async function handleSubmitIA(e: FormEvent) {
+    e.preventDefault();
+
     const res = await api.post(`/message`, {
       message,
     });
